45fa6793a7e9bf43c8415fe9d4f818a7d8395ef3
John Schmidt <john@john2143.com>
2023-04-20 23:45:34 -0400

    remove some warnings, expand some types

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,33 @@
+use std::f32::consts::PI;
+
 use bevy::{
     diagnostic::FrameTimeDiagnosticsPlugin,
     prelude::*,
-    render::render_resource::Face,
-    utils::tracing::instrument::WithSubscriber,
-    window::{self, CursorGrabMode},
 };
 use bevy_asset_loader::prelude::*;
-use bevy_fly_camera::{FlyCameraPlugin};
+use bevy_fly_camera::FlyCameraPlugin;
 use bevy_inspector_egui::quick::WorldInspectorPlugin;
+use bevy_sprite3d::Sprite3dPlugin;
 
 pub mod player;
 pub mod setup;
 pub mod sprites;
 pub mod states;
 pub mod lifetime;
-use bevy_sprite3d::Sprite3dPlugin;
-use player::*;
-use setup::*;
-use sprites::*;
-use states::*;
-use lifetime::*;
 
 pub const HEIGHT: f32 = 720.0;
 pub const WIDTH: f32 = 1280.0;
-pub const PI: f32 = 3.1415926536897932;
 
 fn main() {
     App::new()
         // bevy_sprite3d
-        .add_state::<GameState>()
-        .add_state::<FreeCamState>()
+        .add_state::<states::GameState>()
+        .add_state::<states::FreeCamState>()
         .add_loading_state(
-            LoadingState::new(GameState::Loading).continue_to_state(GameState::Ready),
+            LoadingState::new(states::GameState::Loading).continue_to_state(states::GameState::Ready),
         )
         .add_plugin(Sprite3dPlugin)
-        .add_collection_to_loading_state::<_, PlayerSpriteAssets>(GameState::Loading)
+        .add_collection_to_loading_state::<_, player::PlayerSpriteAssets>(states::GameState::Loading)
         // Background Color
         .insert_resource(ClearColor(Color::hex("212121").unwrap()))
         // Load Assets
@@ -44,7 +37,7 @@ fn main() {
                 .set(WindowPlugin {
                     primary_window: Some(Window {
                         title: "Realm of the OctoSurvivors!".into(),
-                        resolution: (1280., 720.).into(),
+                        resolution: (WIDTH, HEIGHT).into(),
                         // present_mode: window::PresentMode::AutoVsync,
                         resizable: false,
                         // Tells wasm to resize the window according to the available canvas
@@ -56,21 +49,21 @@ fn main() {
                 })
                 .set(ImagePlugin::default_nearest()),
         )
-        .add_startup_systems((spawn_camera, spawn_scene, spawn_tower))
-        .add_system(spawn_player_sprite.run_if(in_state(GameState::Ready).and_then(run_once())))
+        .add_startup_systems((setup::spawn_camera, setup::spawn_scene, spawn_tower))
+        .add_system(player::spawn_player_sprite.run_if(in_state(states::GameState::Ready).and_then(run_once())))
         .add_systems(
-            (player_movement, camera_follow_system)
-            .distributive_run_if(in_state(FreeCamState::Locked)))
+            (player::player_movement, player::camera_follow_system)
+            .distributive_run_if(in_state(states::FreeCamState::Locked)))
         .add_systems(
-            (animate_sprite, face_sprite_to_camera)
-                .distributive_run_if((in_state(GameState::Ready))),
+            (sprites::animate_sprite, sprites::face_sprite_to_camera)
+                .distributive_run_if(in_state(states::GameState::Ready)),
         )
-        .add_system(toggle_freecam)
+        .add_system(states::toggle_freecam)
         .register_type::<Tower>()
         .add_plugin(FrameTimeDiagnosticsPlugin)
         .add_plugin(WorldInspectorPlugin::new())
         //.add_system(tower_shooting)
-        .add_system(lifetime_despawn)
+        .add_system(lifetime::lifetime_despawn)
         // run `setup` every frame while loading. Once it detects the right
         // conditions it'll switch to the next state.
         .run()
@@ -101,7 +94,7 @@ pub struct Tower {
 }
 
 
-fn tower_shooting(
+fn _tower_shooting(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
@@ -120,7 +113,7 @@ fn tower_shooting(
                     transform: spawn_transform,
                     ..default()
                 })
-                .insert(Lifetime {
+                .insert(lifetime::Lifetime {
                     timer: Timer::from_seconds(0.4, TimerMode::Once),
                 })
                 .insert(Name::new("Bullet"));
