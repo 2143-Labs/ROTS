a3ec8fdf09b517e5d50c5f8e84e56a33a49ee254
John Schmidt <john@john2143.com>
2023-04-23 04:25:41 -0400

    server compiles

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,25 @@
+use bevy_log::LogPlugin;
+//use bevy::{TaskPoolPlugin, TypeRegistrationPlugin, FrameCountPlugin, HierarchyPlugin};
+use bevy_time::TimePlugin;
+use bevy_transform::TransformPlugin;
+use bevy_hierarchy::HierarchyPlugin;
+//::diagnostic::DiagnosticsPlugin;
+use bevy_app::App;
+use bevy_core::TaskPoolPlugin;
+use bevy_core::TypeRegistrationPlugin;
+use bevy_diagnostic::DiagnosticsPlugin;
+
 fn main() {
-    println!("hi");
+    let mut app = App::new();
+
+    app
+        .add_plugin(LogPlugin::default())
+        .add_plugin(TaskPoolPlugin::default())
+        .add_plugin(TypeRegistrationPlugin::default())
+        .add_plugin(TimePlugin::default())
+        .add_plugin(TransformPlugin::default())
+        .add_plugin(HierarchyPlugin::default())
+        .add_plugin(DiagnosticsPlugin::default());
+
+    app.run();
 }
