50150f17b2aba3711e545a3b2f10f1a24e0b5466
Eric Bell <mail@ericbell.dev>
2023-04-25 23:20:34 -0600

    Fixed CameraLock

--- client/src/player.rs
+++ client/src/player.rs
@@ -43,6 +43,7 @@ pub struct Player {
     pub looking_at: Vec3,
     pub facing_vel: f32,
     pub velocity: Vec3,
+    pub lock_movement: [Option<Vec2>; 4],
 }
 impl Default for Player {
     fn default() -> Self {
@@ -51,6 +52,7 @@ impl Default for Player {
             looking_at: Vec3::new(10., 10., 10.),
             facing_vel: 0.,
             velocity: Vec3::ZERO,
+            lock_movement: [None; 4],
         }
     }
 }
@@ -100,38 +102,71 @@ pub fn spawn_player_sprite(
 pub const PLAYER_SPEED: f32 = 5.;
 pub fn player_movement(
     mut commands: Commands,
-    mut player_query: Query<(&mut Transform, Entity, &mut Jumper, &Player)>,
+    mut player_query: Query<(&mut Transform, Entity, &mut Jumper, &mut Player)>,
     camera_query: Query<&CameraFollow>,
     keyboard_input: Res<Input<KeyCode>>,
     time: Res<Time>,
 ) {
+    // let old_rotation= Vec2::new(
+        // f32::to_radians(camera_query.single().old_degrees).sin(),
+        // f32::to_radians(camera_query.single().old_degrees).cos()
+    // );
     let rotation= Vec2::new(
-        f32::to_radians(camera_query.single().old_degrees).sin(),
-        f32::to_radians(camera_query.single().old_degrees).cos()
+        f32::to_radians(camera_query.single().degrees).sin(),
+        f32::to_radians(camera_query.single().degrees).cos()
     );
-for(mut transform, player_ent, mut jumper, player) in player_query.iter_mut() {
-    let mut direction = Vec3::ZERO;
-    if keyboard_input.pressed(KeyCode::W) {
-        direction += rotation.xyy() * Vec3::new(-1., 0., -1.);
-        // direction += Vec3::new(-rotation.x, 0., -rotation.y )
-    }
-    if keyboard_input.pressed(KeyCode::S) {
-        direction += rotation.xyy() * Vec3::new(1., 0., 1.);
-    }
-    if keyboard_input.pressed(KeyCode::A) {
-        direction += rotation.perp().xyy() * Vec3::new(1., 0., 1.);
-        // direction += Vec3::new(rotation.perp().x, 0., rotation.perp().y )
-    }
-    if keyboard_input.pressed(KeyCode::D) {
-        direction += rotation.perp().xyy() * Vec3::new(-1., 0., -1.);
-        // direction += Vec3::new(-rotation.perp().x, 0., -rotation.perp().y)
-    }
-    jumper.timer.tick(time.delta());
-    if keyboard_input.pressed(KeyCode::Space) {
-        if jumper.timer.finished() {
-            commands.entity(player_ent).insert(ExternalImpulse {
-                    impulse: Vec3::new(0., 4., 0.),
-                    torque_impulse: Vec3::new(0., 0., 0.),
+    let dragging = camera_query.single().dragging;
+    for(mut transform, player_ent, mut jumper, mut player) in player_query.iter_mut() {
+        if keyboard_input.just_pressed(KeyCode::W){
+            player.lock_movement[0] = Some(rotation);
+        } else if keyboard_input.just_released(KeyCode::W){
+            player.lock_movement[0] = None;
+        }
+        // if keyboard_input.just_pressed(KeyCode::A){
+        //     player.lock_movement[1] = Some(rotation);
+        // } else if keyboard_input.just_released(KeyCode::A){
+        //     player.lock_movement[1] = None;
+        // }
+        if keyboard_input.just_pressed(KeyCode::S){
+            player.lock_movement[2] = Some(rotation);
+        } else if keyboard_input.just_released(KeyCode::S){
+            player.lock_movement[2] = None;
+        }
+        // if keyboard_input.just_pressed(KeyCode::D){
+        //     player.lock_movement[3] = Some(rotation);
+        // } else if keyboard_input.just_released(KeyCode::D){
+        //     player.lock_movement[3] = None;
+        // }
+        let mut direction = Vec3::ZERO;
+        if keyboard_input.pressed(KeyCode::W) {
+            let cur_rotation = if dragging {player.lock_movement[0].unwrap_or(rotation)} else {rotation};
+            direction += cur_rotation
+                .xyy() * Vec3::new(-1., 0., -1.);
+            // direction += Vec3::new(-rotation.x, 0., -rotation.y )
+        }
+        if keyboard_input.pressed(KeyCode::A) {
+            // let cur_rotation = if dragging {player.lock_movement[1].unwrap_or(rotation)} else {rotation};
+            direction += rotation 
+                .perp().xyy() * Vec3::new(1., 0., 1.);
+            // direction += Vec3::new(rotation.perp().x, 0., rotation.perp().y )
+        }
+        if keyboard_input.pressed(KeyCode::S) {
+            let cur_rotation = if dragging {player.lock_movement[2].unwrap_or(rotation)} else {rotation};
+            direction += cur_rotation
+                .xyy() * Vec3::new(1., 0., 1.);
+        }
+        if keyboard_input.pressed(KeyCode::D) {
+            // let cur_rotation = if dragging {player.lock_movement[3].unwrap_or(rotation)} else {rotation};
+            direction += rotation 
+                .perp().xyy() * Vec3::new(-1., 0., -1.);
+            // direction += Vec3::new(-rotation.perp().x, 0., -rotation.perp().y)
+        }
+        jumper.timer.tick(time.delta());
+        if keyboard_input.pressed(KeyCode::Space) {
+            if jumper.timer.finished() {
+                commands.entity(player_ent).insert(ExternalImpulse {
+                        impulse: Vec3::new(0., 4., 0.),
+                        torque_impulse: Vec3::new(0., 0., 0.),
                 });
                 jumper.timer.reset();
             }
@@ -148,33 +183,49 @@ pub fn camera_follow_system(
     mut mouse_events: EventReader<MouseMotion>,
     mouse_input: Res<Input<MouseButton>>,
     mut camera_query: Query<(&mut Transform, &mut CameraFollow), With<Camera3d>>,
-    player_query: Query<&Transform, (With<Player>, Without<CameraFollow>)>,
+    mut player_query: Query<(&Transform, &mut Player), Without<CameraFollow>>,
+    keyboard_input: Res<Input<KeyCode>>,
 ) {
-    for (_, mut camera_follow) in camera_query.iter_mut() {
-        for event in mouse_wheel_events.iter() {
-            camera_follow.distance = match event.y {
-                y if y < 0. => (camera_follow.distance + 1.).abs(),
-                y if y > 0. => (camera_follow.distance - 1.).abs(),
-                _ => camera_follow.distance,
-            };
-            if camera_follow.distance < camera_follow.min_distance {
-                camera_follow.distance = camera_follow.min_distance;
-            } else if camera_follow.distance > camera_follow.max_distance {
-                camera_follow.distance = camera_follow.max_distance;
+    if let Ok((player_transform, mut player)) = player_query.get_single_mut() {
+        for (_, mut camera_follow) in camera_query.iter_mut() {
+            dbg!{player.lock_movement};
+            for event in mouse_wheel_events.iter() {
+                camera_follow.distance = match event.y {
+                    y if y < 0. => (camera_follow.distance + 1.).abs(),
+                    y if y > 0. => (camera_follow.distance - 1.).abs(),
+                    _ => camera_follow.distance,
+                };
+                if camera_follow.distance < camera_follow.min_distance {
+                    camera_follow.distance = camera_follow.min_distance;
+                } else if camera_follow.distance > camera_follow.max_distance {
+                    camera_follow.distance = camera_follow.max_distance;
+                }
+            }
+            if mouse_input.pressed(MouseButton::Right) {
+                camera_follow.dragging = true;
+                for event in mouse_events.iter() {
+                    camera_follow.degrees += event.delta.x;
+                }
+                // player.lock_movement = [None; 4];
+            }
+            if mouse_input.just_released(MouseButton::Right){
+                camera_follow.dragging = false;
+                camera_follow.old_degrees = camera_follow.degrees;
+                player.lock_movement = [None; 4];
+                if keyboard_input.pressed(KeyCode::W) {
+                    player.lock_movement[0] = Some(Vec2::new(
+                        f32::to_radians(camera_follow.degrees).sin(),
+                        f32::to_radians(camera_follow.degrees).cos()
+                    ));
+                }
+                if keyboard_input.pressed(KeyCode::S) {
+                    player.lock_movement[2] = Some(Vec2::new(
+                        f32::to_radians(camera_follow.degrees).sin(),
+                        f32::to_radians(camera_follow.degrees).cos()
+                    ));
+                }
             }
         }
-        if mouse_input.pressed(MouseButton::Right) {
-            camera_follow.dragging = true;
-            for event in mouse_events.iter() {
-                camera_follow.degrees += event.delta.x;
-            }
-        }
-        if mouse_input.just_released(MouseButton::Right){
-            camera_follow.dragging = false;
-            camera_follow.old_degrees = camera_follow.degrees;
-        }
-    }
-    if let Ok(player_transform) = player_query.get_single() {
         for (mut transform, camera_follow) in camera_query.iter_mut() {
         let new_transform= Transform::from_translation(
                 Vec3::new(
