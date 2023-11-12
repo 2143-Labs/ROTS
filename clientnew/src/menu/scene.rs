69bd10c95ceadc0f687df0f73f84b23419e8a4bd
John Schmidt <john_schmidt@discovery.com>
2023-11-12 17:46:13 -0500

    fix naming of var

--- clientnew/src/menu/scene.rs
+++ clientnew/src/menu/scene.rs
@@ -76,21 +76,21 @@ pub fn check_is_next_to_button(
     // This system will add or remove the `SelectedButton` component, and make the buttons spin
     let max_dist = 2.0;
     for (_player_ent, _player, player_transform) in &players {
-        for (button_ent, _button, mut button_transform, selected) in &mut buttons {
+        for (button_ent, button_type, mut button_transform, selected) in &mut buttons {
             if selected.is_some() {
-                button_transform.rotate_x(time.delta_seconds() * 1.5);
-                button_transform.rotate_y(time.delta_seconds() * 3.0);
-                button_transform.rotate_z(time.delta_seconds() * 6.0);
+                button_transform.rotate_x(time.delta_seconds() * 3.0);
+                button_transform.rotate_y(time.delta_seconds() * 1.0);
+                button_transform.rotate_z(time.delta_seconds() * 1.0);
                 let dist = player_transform.translation.distance(button_transform.translation);
                 if dist > max_dist {
-                    info!("Left range of {button_ent:?}");
+                    info!("Left range of {button_type:?}");
                     commands.entity(button_ent).remove::<SelectedButton>();
                 }
             } else {
                 button_transform.rotation = Quat::from_rotation_y(0.0);
                 let dist = player_transform.translation.distance(button_transform.translation);
                 if dist < max_dist {
-                    info!("Approached {button_ent:?}");
+                    info!("Approached {button_type:?}");
                     commands.entity(button_ent).insert(SelectedButton);
                 }
             }
