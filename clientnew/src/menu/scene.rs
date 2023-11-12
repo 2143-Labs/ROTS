935f51109ac983393e901ee9b2529df30151a3ce
John Schmidt <john_schmidt@discovery.com>
2023-11-12 17:40:12 -0500

    clickable menu system

--- clientnew/src/menu/scene.rs
+++ clientnew/src/menu/scene.rs
@@ -1,6 +1,8 @@
-use bevy::{prelude::*, render::{camera::RenderTarget, render_resource::{TextureDimension, TextureDescriptor, TextureFormat, TextureUsages, Extent3d}}};
+use std::f32::consts::PI;
 
-use crate::states::GameState;
+use bevy::{prelude::*, render::{camera::RenderTarget, render_resource::{TextureDimension, TextureDescriptor, TextureFormat, TextureUsages, Extent3d}}, ecs::query::QuerySingleError};
+
+use crate::{states::GameState, cameras::Player};
 
 use super::MenuItem;
 
@@ -8,13 +10,31 @@ pub fn menu_select(
     keyboard_input: Res<Input<KeyCode>>,
     _config: Res<shared::Config>,
     mut game_state: ResMut<NextState<GameState>>,
+    buttons: Query<&MenuButton, With<SelectedButton>>,
 ) {
-    if keyboard_input.pressed(KeyCode::H) {
-        game_state.set(GameState::InGame);
+
+    if !keyboard_input.just_pressed(KeyCode::H) {
+        return;
     }
+
+    let button = match buttons.get_single() {
+        Ok(button) => button,
+        Err(QuerySingleError::NoEntities(_)) => {
+            // Play sound error
+            info!("Not near a button");
+            return;
+        }
+        Err(QuerySingleError::MultipleEntities(_)) => {
+            info!("Somehow got multiple selected buttons?");
+            return;
+        }
+    };
+
+    info!("Clicked button {button:?}");
+    game_state.set(GameState::InGame);
 }
 
-#[derive(Component)]
+#[derive(Component, Debug)]
 pub enum MenuButton {
     Connect,
     Quit,
@@ -44,10 +64,43 @@ impl MenuButton {
     }
 }
 
+#[derive(Component)]
+pub struct SelectedButton;
+
+pub fn check_is_next_to_button(
+    mut commands: Commands,
+    players: Query<(Entity, &Player, &Transform)>,
+    mut buttons: Query<(Entity, &MenuButton, &mut Transform, Option<&SelectedButton>), Without<Player>>,
+    time: Res<Time>
+) {
+    // This system will add or remove the `SelectedButton` component, and make the buttons spin
+    let max_dist = 2.0;
+    for (_player_ent, _player, player_transform) in &players {
+        for (button_ent, _button, mut button_transform, selected) in &mut buttons {
+            if selected.is_some() {
+                button_transform.rotate_x(time.delta_seconds() * 1.5);
+                button_transform.rotate_y(time.delta_seconds() * 3.0);
+                button_transform.rotate_z(time.delta_seconds() * 6.0);
+                let dist = player_transform.translation.distance(button_transform.translation);
+                if dist > max_dist {
+                    info!("Left range of {button_ent:?}");
+                    commands.entity(button_ent).remove::<SelectedButton>();
+                }
+            } else {
+                button_transform.rotation = Quat::from_rotation_y(0.0);
+                let dist = player_transform.translation.distance(button_transform.translation);
+                if dist < max_dist {
+                    info!("Approached {button_ent:?}");
+                    commands.entity(button_ent).insert(SelectedButton);
+                }
+            }
+        }
+    }
+}
+
 pub fn spawn_menu_scene(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
-    mut images: ResMut<Assets<Image>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
     asset_server: ResMut<AssetServer>,
 ) {
@@ -117,9 +170,18 @@ pub fn spawn_menu_scene(
     MenuButton::Quit.spawn(Transform::from_xyz(3.0, 1.0, 0.0), &mut commands, &mut materials, &mut meshes);
     MenuButton::Connect.spawn(Transform::from_xyz(0.0, 1.0, 3.0), &mut commands, &mut materials, &mut meshes);
 
+}
+
+fn _test_sub_render(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut images: ResMut<Assets<Image>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+    asset_server: ResMut<AssetServer>,
+) {
     commands.spawn((
         TextBundle::from_section(
-            "GameState: Menu",
+            "GameState: MASDFASDF",
             TextStyle {
                 font: asset_server.load("fonts/fonts/ttf/JetBrainsMono-Regular.ttf"),
                 font_size: 14.0,
@@ -138,8 +200,8 @@ pub fn spawn_menu_scene(
 
 
     let cam_size = Extent3d {
-        width: 100,
-        height: 100,
+        width: 1000,
+        height: 1000,
         ..default()
     };
 
@@ -165,8 +227,7 @@ pub fn spawn_menu_scene(
 
     let image_handle = images.add(image);
 
-    let camera = Camera3dBundle {
-        transform: Transform::from_xyz(0.0, 3.0, 0.0).looking_at(Vec3::new(3.0, 1.0, 3.0), Vec3::Y),
+    let camera = Camera2dBundle {
         camera: Camera {
             target: RenderTarget::Image(image_handle.clone()),
             ..default()
@@ -186,15 +247,17 @@ pub fn spawn_menu_scene(
         ..default()
     });
 
-    let cube_size = 4.0;
-    let cube_handle = meshes.add(Mesh::from(shape::Box::new(cube_size, cube_size, cube_size)));
+    let cube_size = 1.0;
+    let cube_handle = meshes.add(Mesh::from(shape::Plane { size: cube_size, subdivisions: 4 }));
 
     // Main pass cube, with material containing the rendered first pass texture.
     commands.spawn((
         PbrBundle {
             mesh: cube_handle,
             material: material_handle,
-            transform: Transform::from_xyz(-3.0, 1.0, 0.0),
+            transform: Transform::from_xyz(-3.0, 1.0, 0.0)
+                .with_rotation(Quat::from_rotation_x(PI/2.0))
+                .with_rotation(Quat::from_rotation_y(PI/2.0)),
             ..default()
         },
         MenuItem,
