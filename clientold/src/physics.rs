ab667378f38a1dfada5cfafd419947a08d1d655e
John Schmidt <john@john2143.com>
2023-12-15 20:23:41 -0500

    remove old client, add new server

--- clientold/src/physics.rs
+++
@@ -1,8 +0,0 @@
-use bevy::prelude::*;
-use bevy_rapier3d::prelude::ActiveEvents;
-
-//pub fn modify_collider_active_events(mut active_events: Query<&mut ActiveEvents>) {
-    //for active_event in active_events.iter_mut() {
-        //debug!(?active_event);
-    //}
-//}
