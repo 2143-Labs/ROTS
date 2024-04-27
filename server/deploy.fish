5f205dc7b4949153b4bc3c470e45b6d89f5495b2
John Schmidt <john@john2143.com>
2024-04-27 07:39:24 -0400

    make it a more robust deployment script

--- server/deploy.fish
+++ server/deploy.fish
@@ -1,15 +1,39 @@
 #!/usr/bin/env fish
-cargo r --release &
+set my_pid %self
+echo "Our PID is $my_pid"
+
+function my_signal_handler --on-signal SIGINT
+    echo Got SIGINT signal!
+    unlaunch
+    exit 0
+end
+
+function last_pid
+    pgrep -f "ROTS/target/release/server"
+end
+
+function launch
+    cargo r --release &
+    echo "Starting... "(last_pid)
+end
+
+function unlaunch
+    #kill %ROTS/target/release/server
+    set pid (last_pid)
+    echo "killing $pid"
+    kill $pid
+end
+
+
+launch
 while true do;
-    echo "fetching"
+    echo (last_pid)
     git fetch
     if not test (git rev-parse HEAD) = (git rev-parse @{u});
-        echo "Restarting server: got push"
-        set pid (ps aux | grep ROTS/target/release/server | grep -v "rg ROTS" | choose 1)
-        kill $pid
+        unlaunch
         git pull
-        cargo r --release &
+        launch
     else
-        sleep 5
+        sleep 1
     end
 end
