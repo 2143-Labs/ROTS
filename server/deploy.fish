2b9134983f58bbdca9a180a4a2e349a36e16f2e7
John Schmidt <john_schmidt@discovery.com>
2024-04-27 06:44:01 -0400

    fixup deploy

--- server/deploy.fish
+++ server/deploy.fish
@@ -1,6 +1,8 @@
+#!/usr/bin/env fish
 while true do;
+    echo "fetching"
     git fetch
-    if not test (git rev-parse HEAD) == (git rev-parse @{u});
+    if not test (git rev-parse HEAD) = (git rev-parse @{u});
         echo "Restarting server: got push"
         set pid (ps aux | grep ROTS/target/release/server | grep -v "rg ROTS" | choose 1)
         kill $pid
