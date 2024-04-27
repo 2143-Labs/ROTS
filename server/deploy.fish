ab70b37dd9840ad92ebe5ad2d6113423e860177c
John Schmidt <john@john2143.com>
2024-04-27 06:40:14 -0400

    add deploy.fish

---
+++ server/deploy.fish
@@ -0,0 +1,10 @@
+while true do;
+    git fetch
+    if not test (git rev-parse HEAD) == (git rev-parse @{u});
+        echo "Restarting server: got push"
+        set pid (ps aux | grep ROTS/target/release/server | grep -v "rg ROTS" | choose 1)
+        kill $pid
+        git pull
+        cargo r --release &
+    end
+end
